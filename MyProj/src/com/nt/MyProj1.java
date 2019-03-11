3758ed03c4bd394ed18b8c102037462ca6635f92
maheshgoud419 <46348219+maheshgoud419@users.noreply.github.com>
2019-03-11 08:33:52 +0530

    Update MyProj1.java
    other person commited

--- MyProj/src/com/nt/MyProj1.java
+++ MyProj/src/com/nt/MyProj1.java
@@ -23,5 +23,12 @@ public class MyProj1 {
 		// TODO Auto-generated method stub
 
 	}
+/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
