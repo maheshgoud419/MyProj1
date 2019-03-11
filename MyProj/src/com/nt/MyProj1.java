94d0e53e0720a57f17fd07af51f53944aead75f1
Mahesh <maheshgoud419@gmail.com>
2019-03-11 09:04:31 +0530

    comiteg

--- MyProj/src/com/nt/MyProj1.java
+++ MyProj/src/com/nt/MyProj1.java
@@ -5,19 +5,7 @@ package com.nt;
 
 public class MyProj1 {
 
-	public MyProj1() {
-		
-	}
-
-	public static void main(String[] args) {
-		
-
-	}
-
-	public static void main1(String[] args) {
-		
-
-	}
+	
 public static void main2(String[] args) {
 		
 
