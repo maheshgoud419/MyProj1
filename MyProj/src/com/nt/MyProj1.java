6ec24ed27c1a5d50b242b9cd7f27f2afa1055a69
Dell <Dell@Mahee>
2019-03-11 08:29:07 +0530

    1st commit

---
+++ MyProj/src/com/nt/MyProj1.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.nt;
+
+/**
+ * @author Dell
+ *
+ */
+public class MyProj1 {
+
+	/**
+	 * 
+	 */
+	public MyProj1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
